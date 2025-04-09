78aa9fe1597bdfcc53d9c4609bb8e70914d9ab73
Shah Vishu <142143212+Vishu-shah@users.noreply.github.com>
2025-04-09 12:51:01 +0530

    Update test_feedback_entry.py

--- tests/test_feedback_entry.py
+++ tests/test_feedback_entry.py
@@ -1,4 +1,6 @@
-# tests/test_feedback_entry.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from feedback_entry import add_feedback
 
