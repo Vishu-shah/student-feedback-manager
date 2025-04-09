83b6b1a0e2e7811a7e3016e3a499aadfd57d07e8
Shah Vishu <142143212+Vishu-shah@users.noreply.github.com>
2025-04-09 12:51:41 +0530

    Update test_score_calculator.py

--- tests/test_score_calculator.py
+++ tests/test_score_calculator.py
@@ -1,4 +1,6 @@
-# tests/test_score_calculator.py
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from score_calculator import calculate_average
 
